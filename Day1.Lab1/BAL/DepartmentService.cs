3dfe2f204b7b22066a6226c641ea03bafc38d094
Ahmed Mamdouh Ismael <73735102+ahmadmamdouh-10@users.noreply.github.com>
2021-11-05 14:55:50 +0200

    Add files via upload

---
+++ Day1.Lab1/BAL/DepartmentService.cs
@@ -0,0 +1,37 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAL
+{
+    public class DepartmentService
+    {
+        DbHelper dbHelper = new DbHelper();
+
+        public List<Department> GetDepartments()
+        {
+            List<Department> departments = new List<Department>();
+
+            foreach (DataRow row in dbHelper.GetAllDepartments().Rows)
+            {
+                departments.Add(new Department
+                {
+                    Dnum = (int)row["Dnum"],
+                    Dname = row["Dname"].ToString(),
+                }
+                );
+            }
+
+            return departments;
+        }
+
+        public void AddDepartment(Department department)
+        {
+
+        }
+    }
+}
